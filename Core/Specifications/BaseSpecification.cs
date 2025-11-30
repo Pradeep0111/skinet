dbe7f19b81be2b400122c538a727d25f35765c3c
Pradeep S <pradeep5049@outlook.com>
2025-11-30 22:53:10 +0530

    added projection in specification pattern

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -17,6 +17,8 @@ namespace Core.Specifications
 
         public Expression<Func<T, object>>? OrderByDescending { get; private set; }
 
+        public bool IsDistinct { get; private set; } 
+
         protected void AddOrderBy(Expression<Func<T, object>> OrderByExpression)
         {
             OrderBy = OrderByExpression;
@@ -25,6 +27,24 @@ namespace Core.Specifications
         protected void AddOrderByDescending(Expression<Func<T, object>> OrderByDescExpression)
         {
             OrderByDescending = OrderByDescExpression;
+        }
+
+        protected void ApplyDistinct()
+        {
+            IsDistinct = true;
+        }
+    }
+
+    public class BaseSpecification<T, TResult>(Expression<Func<T, bool>>? criteria) :
+        BaseSpecification<T>(criteria), 
+        ISpecification<T, TResult>
+    {
+        protected BaseSpecification() : this(null) { } 
+        public Expression<Func<T, TResult>>? Select { get; private set; }
+
+        protected void AddSelect(Expression<Func<T, TResult>> SelectExpression)
+        {
+            Select = SelectExpression;
         }
     }
 }
