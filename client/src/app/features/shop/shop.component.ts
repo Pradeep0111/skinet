53caa52f106385ef01024f284588f8f54e2880e3
Pradeep S <pradeep5049@outlook.com>
2026-03-23 15:55:26 +0530

    Enhance Angular app with empty state components and build steps; update Redis connection

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -12,6 +12,7 @@ import { ShopParams } from '../../shared/models/shopParams';
 import { MatPaginator, PageEvent } from "@angular/material/paginator";
 import { Pagination } from '../../shared/models/pagination';
 import { FormsModule } from '@angular/forms';
+import { EmptyStateComponent } from "../../shared/components/empty-state/empty-state.component";
 
 @Component({
   selector: 'app-shop',
@@ -23,7 +24,7 @@ import { FormsModule } from '@angular/forms';
     MatMenuTrigger,
     MatListOption,
     MatPaginator,
-    FormsModule, MatIconButton],
+    FormsModule, MatIconButton, EmptyStateComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
@@ -47,6 +48,10 @@ export class ShopComponent implements OnInit {
   initalizeShop(){
     this.shopService.getBrands();
     this.shopService.getTypes();
+    this.getProducts();
+  }
+  resetFilters(){
+    this.shopParams = new ShopParams();
     this.getProducts();
   }
   
